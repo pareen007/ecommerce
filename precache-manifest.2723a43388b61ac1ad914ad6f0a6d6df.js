b0ac0c4393d81d58f5855a11dfd9adfcd21a0218
Pareen Patel <pareendpatel@gmail.com>
2020-07-21 16:30:12 -0700

    Updates

---
+++ precache-manifest.2723a43388b61ac1ad914ad6f0a6d6df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec5d964ab07d8dcd1856f0620f88ecae",
+    "url": "/ecommerce/index.html"
+  },
+  {
+    "revision": "279f2fb26a59dbf0a6c1",
+    "url": "/ecommerce/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "275c99dff981f5510a90",
+    "url": "/ecommerce/static/js/2.f077ba3d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ecommerce/static/js/2.f077ba3d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "279f2fb26a59dbf0a6c1",
+    "url": "/ecommerce/static/js/main.5ac0adbd.chunk.js"
+  },
+  {
+    "revision": "8c9ff8e64a0decf7fb42",
+    "url": "/ecommerce/static/js/runtime-main.9d1641c3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ecommerce/static/media/logo.5d5d9eef.svg"
+  }
+]);
